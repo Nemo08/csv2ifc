e8866230c8e8bdc52188787e026e0dbb622f7db0
Nemo08 <bigblacklazycat@gmail.com>
2022-05-17 17:19:20 +0300

    Encoded UTF charc to upper hex
    Solibri dont understand lowwer letters in hex encoding

--- internal/encoding.go
+++ internal/encoding.go
@@ -3,6 +3,7 @@ package internal
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 const (
@@ -13,9 +14,10 @@ const (
 // Encode2HexString encode UTF8 string to string in UTF IFC format
 func Encode2HexString(in string) (out string, err error) {
 	var a string
+	fmt.Println(in)
 	for _, rv := range in {
 		if int(rv) > 126 {
-			a = a + `\X2\` + fmt.Sprintf("%04s", strconv.FormatInt(int64(rv), 16)) + `\X0\`
+			a = a + `\X2\` + strings.ToUpper(fmt.Sprintf("%04s", strconv.FormatInt(int64(rv), 16))) + `\X0\`
 		} else {
 			a = a + string(rv)
 		}
